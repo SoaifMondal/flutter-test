f3374d2b0b51ac2c5bf725d289b24a7a6d051beb
Soaif Ikbal Mondal <107474186+Soaif0912@users.noreply.github.com>
2024-11-18 13:15:47 +0000

    Custom input widget

---
+++ lib/widgets/custom_input_widget.dart
@@ -0,0 +1,103 @@
+
+import 'package:flutter/material.dart';
+
+class CustomInputWidget extends StatefulWidget{
+
+  final TextEditingController controller;
+  final String hintText;
+  final bool isPassword;
+  final String? Function(String?)? validator;
+  final double height;
+  final double width;
+  final IconData? inputIcon;
+
+  const CustomInputWidget({
+    required this.controller,
+    required this.hintText,
+    this.isPassword = false,
+    this.validator,
+    required this.height,
+    required this.width,
+    this.inputIcon,
+    super.key
+  });
+
+  @override
+  CustomInputWidgetState createState() => CustomInputWidgetState();
+}
+
+class CustomInputWidgetState extends State<CustomInputWidget>{
+
+  bool _obscureText = true;
+  String? _errorText;
+
+  void validate(){
+    setState(() {
+      _errorText = widget.validator != null ? widget.validator!(widget.controller.text) : null;
+    });
+  }
+  @override
+  Widget build(BuildContext context) {
+    return
+    Column(
+      children: [
+        Container(
+          width: widget.width,
+          height: widget.height,
+          decoration: BoxDecoration(
+            color: Colors.white,
+            borderRadius: BorderRadius.circular(100), 
+            border: Border.all(color: Color.fromRGBO(11, 34, 62, 0.2), width: 1),
+          ),
+          child: TextFormField(
+            controller: widget.controller,
+            onChanged: (value) {
+              setState(() {
+                _errorText = widget.validator != null ? widget.validator!(value) : null;
+              });
+            },
+            decoration: InputDecoration(
+              hintText: widget.hintText, // Use hintText instead of labelText
+              hintStyle: const TextStyle(
+                color: Color.fromRGBO(75, 75, 75, 1),
+                fontWeight: FontWeight.w400,
+              ),
+              border: InputBorder.none,
+              contentPadding: EdgeInsets.symmetric(horizontal: 20, vertical: (widget.height - 25) / 2, ),
+              suffixIcon: 
+                widget.isPassword ? 
+                  Padding(
+                    padding: const EdgeInsets.only(right: 30),
+                    child: IconButton(
+                      onPressed: (){
+                        setState(() {
+                          _obscureText = !_obscureText;
+                        });
+                      },
+                      icon: Icon(
+                        _obscureText ? Icons.visibility_outlined : Icons.visibility_off_outlined,
+                        color: const Color.fromRGBO(75, 75, 75, 1),
+                      ),
+                    ),
+                  ) : 
+                  widget.inputIcon != null ?
+                  Padding(
+                    padding: EdgeInsets.only(right: 15),
+                    child: IconButton(onPressed: null, icon: Icon(widget.inputIcon)),
+                  ) : null,
+            ),
+            obscureText: widget.isPassword && _obscureText,
+          ),
+        ),
+        if (_errorText != null)
+          Padding(
+            padding: const EdgeInsets.symmetric(vertical: 5, horizontal: 30),
+            child: Text(
+              _errorText!,
+              style: const TextStyle(color: Colors.red),
+            ),
+          ),
+      ],
+    );
+  }
+}
